8af83ad31438fdf610346f6f4f9498eb18b2e954
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2015-04-12 02:28:58 +0000

    AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,10 @@ Rails.application.configure do
   :storage => :s3,
   :s3_credentials => 
   {
-    :bucket => 'mlnews', #ENV['S3_BUCKET_NAME_TATTOO'],
+    :bucket => 'ml-news', #ENV['S3_BUCKET_NAME_TATTOO'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_host_name => 's3-us-east-1.amazonaws.com'
+    :s3_host_name => 's3-us-west-1.amazonaws.com'
   }
   }
 end
