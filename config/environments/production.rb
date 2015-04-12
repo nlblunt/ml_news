6b0cbc60cb368afb4178ac00c5bfc02f06253d03
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2015-04-12 00:45:19 +0000

    Update s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
     :bucket => 'mlnews', #ENV['S3_BUCKET_NAME_TATTOO'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_host_name => 's3-us-west-2.amazonaws.com'
+    :s3_host_name => 's3-us-east-1.amazonaws.com'
   }
   }
 end
